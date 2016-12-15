02f667f4dc3ff3402fe51303b1bc428a7e693aee
Orest <Orest@linux-4ru6.site>
2016-12-15 11:35:46 +0200

    onSmarty_second_commit

--- models/createtables.php
+++ models/createtables.php
@@ -1,28 +1,28 @@
 <?php
 
-include"connect.php";
+include"class.Connect.php";
 
 echo("***");
 
 $table='REPORTS';
 
-mysql_query("CREATE TABLE IF NOT EXISTS $table(ID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))");
-mysql_query("ALTER TABLE $table ADD NAME TEXT NOT NULL");
-mysql_query("ALTER TABLE $table ADD TITLE TEXT NOT NULL");
-mysql_query("ALTER TABLE $table ADD CONTENT TEXT NOT NULL");
-mysql_query("ALTER TABLE $table ADD DATE DATETIME NOT NULL");
-mysql_query("ALTER TABLE $table ADD COMNUM INTEGER NOT NULL");
-mysql_query("ALTER TABLE $table ADD LAST_COMMENT_DATE DATETIME NOT NULL");
+$con->query("CREATE TABLE IF NOT EXISTS $table(ID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))");
+$con->query("ALTER TABLE $table ADD NAME TEXT NOT NULL");
+$con->query("ALTER TABLE $table ADD TITLE TEXT NOT NULL");
+$con->query("ALTER TABLE $table ADD CONTENT TEXT NOT NULL");
+$con->query("ALTER TABLE $table ADD DATE DATETIME NOT NULL");
+$con->query("ALTER TABLE $table ADD COMNUM INTEGER NOT NULL");
+$con->query("ALTER TABLE $table ADD LAST_COMMENT_DATE DATETIME NOT NULL");
 
 echo ("Table REPORTS has been created.");
 
 $table2='COMMENTS';
 
-mysql_query("CREATE TABLE IF NOT EXISTS $table2(ID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))");
-mysql_query("ALTER TABLE $table2 ADD REPORT_ID varchar(50) NOT NULL");
-mysql_query("ALTER TABLE $table2 ADD NAME varchar(50) NOT NULL");
-mysql_query("ALTER TABLE $table2 ADD CONTENT text NOT NULL");
-mysql_query("ALTER TABLE $table2 ADD DATE DATETIME NOT NULL");
+$con->query("CREATE TABLE IF NOT EXISTS $table2(ID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))");
+$con->query("ALTER TABLE $table2 ADD REPORT_ID varchar(50) NOT NULL");
+$con->query("ALTER TABLE $table2 ADD NAME varchar(50) NOT NULL");
+$con->query("ALTER TABLE $table2 ADD CONTENT text NOT NULL");
+$con->query("ALTER TABLE $table2 ADD DATE DATETIME NOT NULL");
 
 
 echo ("Table COMMENTS has been created.");
